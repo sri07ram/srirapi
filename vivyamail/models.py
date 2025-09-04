da5126d09011cb3ad7e73d171e8311f7b2668917
sri07ram <sriram@vivyacorp.com>
2025-09-04 14:23:29 +0530

    update message

--- vivyamail/models.py
+++ vivyamail/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Mail(models.Model):
-    mail=models.EmailField()
+    mail=models.EmailField()  #mailname
     name=models.CharField(max_length=100)
     mobile=models.CharField(max_length=12)
     place=models.CharField(max_length=100)
